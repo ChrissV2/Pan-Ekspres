96ff0432efe58a46d90704638844c8904596aa88
Krystian Chrzanowski <krchr@wp.p>
2023-10-08 22:00:17 +0200

    added all files

---
+++ src/js/script.js
@@ -0,0 +1,54 @@
+const burgerBtn = document.querySelector('.burger-btn')
+const navBar = document.querySelector('nav')
+const navBtns = document.querySelectorAll('.nav__item')
+const navBtnBars = document.querySelector('.burger-btn__bars')
+const allSections = document.querySelectorAll('.section')
+const footerYear = document.querySelector('.footer__year')
+
+function active() {
+	navBar.classList.toggle('nav--active')
+
+	navBtnBars.classList.remove('black-bars-color')
+
+	navBtns.forEach(item => {
+		item.addEventListener('click', () => {
+			navBar.classList.remove('nav--active')
+		})
+	})
+
+	handleNavItemsAnimation()
+}
+
+const handleNavItemsAnimation = () => {
+	let delayTime = 0
+
+	navBtns.forEach(item => {
+		item.classList.toggle('nav-items-animation')
+		item.style.animationDelay = '.' + delayTime + 's'
+		delayTime++
+	})
+}
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear()
+	footerYear.innerText = year
+}
+
+const handleObserver = () => {
+	const currentSection = window.scrollY
+
+	allSections.forEach(section => {
+
+		if (section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.add('black-bars-color')
+		} else if (!section.classList.contains('white-section') && section.offsetTop <= currentSection + 60) {
+			navBtnBars.classList.remove('black-bars-color')
+		}
+
+
+	})
+}
+
+handleCurrentYear()
+burgerBtn.addEventListener('click', active)
+window.addEventListener('scroll', handleObserver)
