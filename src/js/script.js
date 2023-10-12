dc798176bcacaaa9f4a4a4ca1ac232549a8b2af5
Krystian Chrzanowski <krchr@wp.p>
2023-10-12 18:20:47 +0200

    Added mail

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,151 @@
-const burgerBtn = document.querySelector('.burger-btn')
-const navBar = document.querySelector('nav')
-const navBtns = document.querySelectorAll('.nav__item')
-const navBtnBars = document.querySelector('.burger-btn__bars')
-const allSections = document.querySelectorAll('.section')
-const footerYear = document.querySelector('.footer__year')
-const callBtn = document.querySelector('.contact__details-item--call')
-const navBtn = document.querySelector('.contact__details-item--navigate')
+let burgerBtn
+let navBar
+let navBtns
+let navBtnBars
+let allSections
+let footerYear
+let callBtn
+let navBtn
+let body
+let modal
+let sendBtn
+let closeModalBtn
+let nameInput
+let phoneInput
+let mailInput
+let textArea
+let errorMsgPhone
+let errorMsgName
+let errorMsgMail
+let errorMsgTextArea
+let errorCount = 0
+
+const main = () => {
+	prepareDOMElements()
+	prepareDOMEvents()
+	handleCurrentYear()
+}
+
+const prepareDOMElements = () => {
+	burgerBtn = document.querySelector('.burger-btn')
+	navBar = document.querySelector('nav')
+	navBtns = document.querySelectorAll('.nav__item')
+	navBtnBars = document.querySelector('.burger-btn__bars')
+	allSections = document.querySelectorAll('.section')
+	footerYear = document.querySelector('.footer__year')
+	callBtn = document.querySelector('.contact__details-item--call')
+	navBtn = document.querySelector('.contact__details-item--navigate')
+	body = document.querySelector('body')
+	modal = document.querySelector('.contact__modal')
+	sendBtn = document.querySelector('.contact__form-btn')
+	closeModalBtn = document.querySelector('.contact__modal-closeBtn')
+	nameInput = document.getElementById('name')
+	phoneInput = document.getElementById('number')
+	mailInput = document.getElementById('email')
+	textArea = document.querySelector('.contact__form-textarea')
+	errorMsgName = document.querySelector('.contact__form-errorMsg--name')
+	errorMsgPhone = document.querySelector('.contact__form-errorMsg--phone')
+	errorMsgMail = document.querySelector('.contact__form-errorMsg--mail')
+	errorMsgTextArea = document.querySelector('.contact__form-errorMsg--textArea')
+}
+
+const prepareDOMEvents = () => {
+	burgerBtn.addEventListener('click', active)
+	window.addEventListener('scroll', handleObserver)
+	callBtn.addEventListener('click', handleCallButton)
+	navBtn.addEventListener('click', handleNavButton)
+	sendBtn.addEventListener('click', function (event) {
+		event.preventDefault()
+	})
+	sendBtn.addEventListener('click', countErrors)
+	closeModalBtn.addEventListener('click', closeModal)
+}
+
+
+const closeModal = () => {
+	modal.classList.remove('contact__modal--active')
+}
+
+const clearInputs = () => {
+	nameInput.value = ''
+	phoneInput.value = ''
+	mailInput.value = ''
+	textArea.value = ''
+}
+
+const validateName = () => {
+	if (nameInput.value === '') {
+		errorMsgName.classList.add('contact__form-errorMsg--active')
+		errorCount++
+	} else {
+		errorMsgName.classList.remove('contact__form-errorMsg--active')
+	}
+}
+
+const validatePhoneNumber = () => {
+	const re = /^[0-9\s]*$/
+	
+	if (re.test(phoneInput.value) && phoneInput.value !== '') {
+		errorMsgPhone.classList.remove('contact__form-errorMsg--active')
+	} else {
+		errorMsgPhone.classList.add('contact__form-errorMsg--active')
+		errorCount++
+	}
+}
+
+const validateMail = () => {
+	const reg =
+	/^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,4})$/i
+	
+	if(reg.test(mailInput.value)) {
+		errorMsgMail.classList.remove('contact__form-errorMsg--active')
+	} else {
+		errorMsgMail.classList.add('contact__form-errorMsg--active')
+		errorCount++
+	}
+}
+
+const validateTextArea = () => {
+	if (textArea.value === '') {
+		errorMsgTextArea.classList.add('contact__form-errorMsg--active')
+		errorCount++
+	} else {
+		errorMsgTextArea.classList.remove('contact__form-errorMsg--active')
+	}
+}
+
+const countErrors = () => {
+	validateName()
+	validatePhoneNumber()
+	validateMail()
+	validateTextArea()
+
+
+	if(errorCount !== 0) {
+		errorCount = 0
+		return
+	} else if (errorCount === 0 && document.location.search === '?mail_status=sent') {
+		modal.classList.add('contact__modal--active')
+		clearInputs()
+		errorCount = 0
+	} else if (errorCount === 0 && document.location.search === '?mail_status=error') {
+		console.log('nie udało się wysłać');
+	}
+
+}
 
 function active() {
 	navBar.classList.toggle('nav--active')
-
+	
 	navBtnBars.classList.remove('black-bars-color')
+	navBtnBars.classList.remove('grey-bars-color')
+	
+	body.classList.toggle('overflowHidden')
 
 	navBtns.forEach(item => {
 		item.addEventListener('click', () => {
 			navBar.classList.remove('nav--active')
+			body.classList.remove('overflowHidden')
 			handleNavItemsAnimation()
 		})
 	})
@@ -64,8 +195,4 @@ const handleObserver = () => {
 	})
 }
 
-handleCurrentYear()
-burgerBtn.addEventListener('click', active)
-window.addEventListener('scroll', handleObserver)
-callBtn.addEventListener('click', handleCallButton)
-navBtn.addEventListener('click', handleNavButton)
+document.addEventListener('DOMContentLoaded', main)
